b6cf2d739e1f40c79a867eaf675a8b60b54a7f5c
Omar Abdelaziz <oabdelaziz@2pegypt.local>
2024-09-10 12:26:47 +0300

    add colors

--- lib/core/global/app_colors.dart
+++ lib/core/global/app_colors.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const Color primaryColor =  Color(0xFF36737B);
-  static const Color mainFontColor = Color(0xFF103A20);
-  static const Color bgColor = Color(0xffFCFCFD);
+  static const Color secondaryColor = Color(0xFF03DAC6);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color error = Color(0xFFB00020);
   static const Color fontColor = Color(0xff4A4A4A);
   static const Color whiteColor = Color(0xFFffffff);
-
   static const Color black = Color(0xff000000);
-  static const Color error = Color(0xddf60000);
   static const Color hintColor = Color(0xddaea1a1);
 }
